98481bf970489b50d3cd235141ce4d61d9fac689
Daniel Sørenstam <rcnukebooom@gmail.com>
2017-03-06 10:58:51 +0100

    Patrol
    Started a patrol script that uses raycast to see if there still is
    ground in front of the mob

---
+++ Assets/_Scripts/simplaePatrol.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class simplaePatrol : MonoBehaviour {
+
+    private GameObject body;
+    public float moveSpeed;
+    private Transform stillground;
+	// Use this for initialization
+	void Start ()
+    {
+        body = GameObject.Find(this);
+        stillground = body.GetComponentInChildren < 1 > ();
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
