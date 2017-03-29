6f510399dd994782a81a2d826258a55f36d6466c
Daniel Sørenstam <rcnukebooom@gmail.com>
2017-03-29 16:47:29 +0200

    gamecontroller started
    started work on gamecontroller

---
+++ Assets/_Scripts/gameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameController : MonoBehaviour {
+
+    public Transform[] spawnPoints;
+    public Collider[] spawnTrigger;
+
+    //player info
+    private float health;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
