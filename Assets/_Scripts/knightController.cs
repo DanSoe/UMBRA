f74e273a77926b22868362165998817c194535df
Daniel Sørenstam <rcnukebooom@gmail.com>
2017-04-27 11:29:25 +0200

    enemykknight

---
+++ Assets/_Scripts/knightController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class knightController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public Rigidbody body;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        body.velocity = transform.forward * moveSpeed;
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "edge")
+        {
+            body.transform.rotation = Quaternion.AngleAxis(180, transform.up) * transform.rotation;
+        }
+    }
+}
