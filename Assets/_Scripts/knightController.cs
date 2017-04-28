95c654cdc5de85cfda8f3deffb021930795e54c1
Daniel Sørenstam <rcnukebooom@gmail.com>
2017-04-28 18:17:53 +0200

    kinght lvl up

--- Assets/_Scripts/knightController.cs
+++ Assets/_Scripts/knightController.cs
@@ -7,6 +7,9 @@ public class knightController : MonoBehaviour
     public float moveSpeed;
     public Rigidbody body;
 
+    public GameObject target;
+
+    public float eneDist;
     Animator Anim;
 
     // Use this for initialization
@@ -19,6 +22,22 @@ public class knightController : MonoBehaviour
     void Update()
     {
         Anim.SetFloat("Speed", moveSpeed);
+
+        if (target != null)
+        {
+            eneDist = Vector3.Distance(body.position, target.transform.position);
+
+            if (eneDist < 5f)
+            {
+                Anim.SetBool("Attack", true);
+                Anim.SetFloat("Speed", 0f);
+            }
+            if (eneDist > 5f)
+            {
+                Anim.SetBool("Attack", false);
+                Anim.SetFloat("Speed", moveSpeed);
+            }
+        }
     }
     void FixedUpdate()
     {
