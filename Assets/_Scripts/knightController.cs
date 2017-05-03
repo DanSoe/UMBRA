4896f41612b2b9594284537c4088f8419aa1dfb3
frekva <frekva@hotmail.com>
2017-05-03 17:48:45 +0200

    Obligatory

--- Assets/_Scripts/knightController.cs
+++ Assets/_Scripts/knightController.cs
@@ -103,7 +103,7 @@ public class knightController : MonoBehaviour
     {
         if (buildupMovement)
         {
-            body.InstantForce(Movement);
+           // body.InstantForce(Movement);
             //player.AddForce(transform.forward * MoveSpeed);
             buildupMovement = false;
         }
