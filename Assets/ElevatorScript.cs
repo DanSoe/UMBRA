f6ca9ac7776b4faf2cd561127ec3425a7f1b55de
frekva <frekva@hotmail.com>
2017-05-04 15:00:14 +0200

    Elevators implimented
    Elevators are now implimented instead of using teleport boxes, can still
    adjust the speed of the elevator

---
+++ Assets/ElevatorScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorScript : MonoBehaviour
+{
+    public float speed;
+
+    private int direction = 1;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.up * speed * direction * Time.deltaTime);
+
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Target")
+        {
+            speed = 0;
+            direction = 0;
+        }
+        if (other.tag == "Player")
+        {
+            speed = 10;
+        }
+    }
+     /*
+    void OnCollisionEnter(Collision coli)
+    {
+        if(coli.gameObject.CompareTag("Player"))
+        {
+            
+            
+        }
+    }
+      * */
+}
