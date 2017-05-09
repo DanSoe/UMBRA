e541024216675187c96e0c094d2e29041b442c91
Daniel Sørenstam <rcnukebooom@gmail.com>
2017-05-09 18:46:28 +0200

    walk tweak

--- Assets/_Scripts/GhoulController.cs
+++ Assets/_Scripts/GhoulController.cs
@@ -51,10 +51,10 @@ public class GhoulController : MonoBehaviour
         temp2 = spreadAngle * noAngle;
         temp1 = spreadAngle2 * noAngle;
 
-        curSpeed = ghoul.velocity.magnitude;
+        //curSpeed = ghoul.velocity.magnitude;
         IsAlive = true;
         playerAnim.SetBool("IsAlive", IsAlive);
-        playerAnim.SetFloat("Speed", curSpeed);
+        
     }
 
     void Awake()
@@ -66,12 +66,15 @@ public class GhoulController : MonoBehaviour
 
 	void Update () 
     {
-	
-	}
+
+        //Debug.Log(ghoul.velocity.magnitude);
+        Debug.Log(curSpeed);
+    }
 
     void FixedUpdate()
     {
-
+        playerAnim.SetFloat("Speed", ghoul.velocity.magnitude);
+        curSpeed = ghoul.velocity.magnitude;
         RaycastHit rayOut;
 
         // detecting if the player is in front of the knight.
