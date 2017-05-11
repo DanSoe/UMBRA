0d0b44e5d29e1d4db418236860e1960624170f8d
frekva <frekva@hotmail.com>
2017-05-11 16:46:07 +0200

    Health pickups and porcullilous trap
    - Health pickups now spin again
    - The portcullious trap now works as intended

---
+++ Assets/_Scripts/Portcullious_trap.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portcullious_trap : MonoBehaviour
+{
+    private PlayerController player;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider port)
+    {
+        if (port.gameObject.CompareTag("Player"))
+        {
+            player.takeDamage(2);
+            StartCoroutine(player.Knockback(0.02f, 2550, player.transform.position, transform.position));
+        }
+    }
+}
