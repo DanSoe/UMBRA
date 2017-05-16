6722fb4004e3ee0ed10f8ca9ee707ff50cf49f0a
frekva <frekva@hotmail.com>
2017-05-16 15:32:27 +0200

    Spike trap

---
+++ Assets/_Scripts/SpikeTrap.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeTrap : MonoBehaviour
+{
+    Animator animator;
+    // Use this for initialization
+    void Start()
+    {
+        if (!animator) 
+        { 
+            animator = transform.GetComponentInChildren<Animator>();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider spikes)
+    {
+        if((spikes.gameObject.tag == "Player"))
+        {
+            animator.SetBool("TrapActivator", true);
+        }
+    }
+    void OnTriggerExit(Collider exit)
+    {
+        if ((exit.gameObject.tag == "Player"))
+        {
+            animator.SetBool("TrapActivator", false);
+        }
+    }
+
+}
---
+++ Assets/_Scripts/SpikeTrapDamage.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeTrapDamage : MonoBehaviour
+{
+    private PlayerController player;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider spikedmg)
+    {
+        if (spikedmg.gameObject.CompareTag("Player"))
+        {
+            player.takeDamage(1);
+            StartCoroutine(player.Knockback(0.02f, 2550, player.transform.position, transform.position));
+        }
+    }
+}
