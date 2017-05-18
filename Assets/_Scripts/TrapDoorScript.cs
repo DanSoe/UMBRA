64c9c76c8c2cbf6f758b62bdb980fe77d04394d9
frekva <frekva@hotmail.com>
2017-05-18 17:43:08 +0200

    Textures n stuff

---
+++ Assets/_Scripts/TrapDoorScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapDoorScript : MonoBehaviour
+{
+    Animator anim;
+    public BoxCollider left;
+    public BoxCollider right;
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator OnTriggerEnter(Collider trapdoor)
+    {
+        if ((trapdoor.gameObject.tag == "Player"))
+        {
+            anim.SetBool("TrapActivator", true);
+            yield return new WaitForSeconds(2);
+            left.enabled = false;
+            right.enabled = false;
+        }
+    }
+}
