a9d69fb112c219cce4d08a8755d773d6b8a2a1c6
frekva <frekva@hotmail.com>
2017-05-24 15:30:30 +0200

    Swinging blade trap

---
+++ Assets/_Scripts/SwingingBladeRandom.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwingingBladeRandom : MonoBehaviour
+{
+    Animator swing;
+
+    void Start()
+    {
+        swing = GetComponent<Animator>();
+
+        swing.Play("Swing", 0, Random.value);
+
+    }
+}
